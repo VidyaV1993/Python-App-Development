00418ced2c78f3538f41f8c8cee890728948d7a6
Vidya Viswanathan <50157188+VidyaV1993@users.noreply.github.com>
2022-05-24 16:03:29 -0700

    changes

--- 2-App2-Motion_Detection/plotting.py
+++ 2-App2-Motion_Detection/plotting.py
@@ -1,7 +1,8 @@
 from motion_detector import df 
 from bokeh.plotting import figure, output_file, show
+import pandas
 
-p = figure(width=100, height=100, x_axis_type = "datetime", title="Motion Graph")
+p = figure(width=100, height=100, x_axis_type = "datetime", title="Motion Graph", sizing_mode='scale_width')
 q = p.quad(left=df["Start"], right=df["End"], bottom=0, top=1, color="green")
 
 output_file("Graph.html")
