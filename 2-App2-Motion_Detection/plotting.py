549d8ccd11522e3000c1cf50a42bc216faa5fc0c
Vidya Viswanathan <50157188+VidyaV1993@users.noreply.github.com>
2022-05-24 16:36:34 -0700

    changes in plotting - added hover feature

--- 2-App2-Motion_Detection/plotting.py
+++ 2-App2-Motion_Detection/plotting.py
@@ -1,9 +1,22 @@
 from motion_detector import df 
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool, ColumnDataSource
 import pandas
 
-p = figure(width=100, height=100, x_axis_type = "datetime", title="Motion Graph", sizing_mode='scale_width')
-q = p.quad(left=df["Start"], right=df["End"], bottom=0, top=1, color="green")
+df["Start"] = pandas.to_datetime(df["Start"])
+df["End"] = pandas.to_datetime(df["End"])
+df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds=ColumnDataSource(df)
 
-output_file("Graph.html")
+p = figure(width=100, height=100, x_axis_type = "datetime", title="Motion Graph", sizing_mode='scale_width')
+p.yaxis.minor_tick_line_color=None
+p.ygrid.ticker = [0,1]
+
+hover = HoverTool(tooltips=[("Start", "@Start_string"), ("End", "@End_string")])
+p.add_tools(hover)
+
+q = p.quad(left="Start", right="End", bottom=0, top=1, color="green", source=cds)
+
+output_file("Graph_New.html")
 show(p)
