7861b38f452dd29c405bc069e84f376de7910c7f
Vidya Viswanathan <vidyamsee.uw@gmail.com>
2022-05-27 14:59:47 -0700

    sample commit using git extensions - basics.py - pluralsight course

---
+++ 0-Basics/Practical Python for Beginners/basic.py
@@ -0,0 +1,11 @@
+# Tax calculator program
+amount=100
+tax=0.06
+total=amount+amount*tax
+print(total)
+
+# Age calculator program
+age = int(input("How old are you?\n"))
+decades=age//10
+years=age%10
+print("You are "+ str(decades) +" decades and " + str(years) + " years old.")
